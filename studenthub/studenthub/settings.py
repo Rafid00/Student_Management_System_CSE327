b9d0d7cceb0d8e9452499d6c41dbbea432eb4dfb
Tareq Mahmud <mtareq880@gmail.com>
2021-12-16 14:15:51 +0600

    database switched to sqlite

--- studenthub/studenthub/settings.py
+++ studenthub/studenthub/settings.py
@@ -85,17 +85,9 @@ WSGI_APPLICATION = 'studenthub.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'studenthub',
-        'USER': 'studenthub',
-        'PASSWORD':'studenthub_password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
