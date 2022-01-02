2e434b5dd017dc2313634e1bbdc2f2b272b3b3a5
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2022-01-03 03:07:22 +0600

    update feature of major overview

--- studenthub/student_management_app/views.py
+++ studenthub/student_management_app/views.py
@@ -273,6 +273,6 @@ def grade_overview(request):
     return render(request,"grade_overview.html")
 
 def major_overview (request):
-    contex={'courses':['cse 231','cse 225','cse 115']}
-    return render(request,"major_overview.html",contex)
+    context={'courses':[]}
+    return render(request,"major_overview.html",context)
 
